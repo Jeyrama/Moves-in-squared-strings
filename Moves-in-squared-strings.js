c5373877b5fa89946a2ae8b9f052ea3356b9dad6
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-06 10:17:21 -0700

    Update Moves-in-squared-strings.js

--- Moves-in-squared-strings.js
+++ Moves-in-squared-strings.js
@@ -21,3 +21,18 @@ Task:
 
 
 // Solution
+
+function vertMirror(str) {
+  return str.split('\n')
+    .map(line => line.split('').reverse().join(''))
+    .join('\n')
+}
+function horMirror(str) {
+  return str.split('\n')
+    .reverse()
+    .join('\n')
+}
+function oper(fct, s) {
+  return fct(s)
+}
+
