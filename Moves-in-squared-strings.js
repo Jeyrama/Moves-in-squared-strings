34df5b1d72304333865e1cd5e5eb78d42d1737c4
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-06 10:18:10 -0700

    Update Moves-in-squared-strings.js

--- Moves-in-squared-strings.js
+++ Moves-in-squared-strings.js
@@ -37,3 +37,7 @@ function oper(fct, s) {
 }
 
 // or
+
+const vertMirror = s => s.map(s => [...s].reverse().join(''));
+const horMirror = s => s.reverse();
+const oper = (fct, s) => fct(s.split("\n")).join("\n");
